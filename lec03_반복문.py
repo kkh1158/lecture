0f38c52ee8a837e3a17a245becd381ff681a0270
kkh1158 <error-_-@nate.com>
2023-01-30 17:47:38 +0900

    addlec03

--- lec03_반복문.py
+++ lec03_반복문.py
@@ -122,3 +122,28 @@ for k in range(4):
         else:
             print('*', end='')
     print()
+
+
+
+
+
+
+# while 조건식:
+#     반복실행문
+
+num=5
+while num<10:
+    print('그렇다', num)
+    num=num+1
+print(num)
+
+# while문 구구단
+dan=2
+while dan<=9:
+    gob=1
+    print(dan, '단', sep='')
+    while gob<=9:
+        print(dan, 'X', gob, '=', dan * gob, end='\t')
+        gob=gob+1
+    dan=dan+1
+    print()
